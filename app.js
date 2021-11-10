08f050f46d54c5992a1f167c2769796469f7821e
Canzalo <44494765+canzalo@users.noreply.github.com>
2021-11-10 08:39:39 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+/*
+const app=Vue.createApp({
+    data(){
+        return{
+            logo:'logo',
+            about:'<a href="about"> About Us </a>',
+            contact:'<a href="contact">Contact </a>',
+            blog:'<a href="blog">Blogs </a>',
+            Title:'Afrique Dimensions'
+           
+         
+
+        }
+    }
+  
+})
+app.mount('#app')
+
+*/
